69c2caef20356012ce778a94aad6c0c130423902
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2024-01-01 17:02:04 +0530

    chore: production config and remove debug js files
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

---
+++ webpack.prod.js
@@ -0,0 +1,38 @@
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+    entry: "./src/index.ts",
+    mode: 'production',
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+            { test: /\.tsx?$/, loader: "ts-loader" },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { test: /\.js$/, loader: "source-map-loader" },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: 'src/index.html', favicon: 'src/assets/images/favicon.ico' }),
+        new CopyPlugin({
+            patterns: [
+                { from: "src/assets/wasm", to: "assets/wasm" },
+                { from: "src/assets/demo", to: "assets/demo" },
+            ],
+        }),
+    ],
+    // Other options...
+};
