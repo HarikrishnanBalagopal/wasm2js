706f6bc142a9feeb996213b14d16cda9d5545cae
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2024-02-02 18:27:50 +0530

    chore: change deterministic seedable prng back to Math.random
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- demo/src/demo/match3aot.ts
+++ demo/src/demo/match3aot.ts
@@ -2,42 +2,42 @@ import { ELEM_ID } from "./common";
 import { instantiateAot } from "@haribala/wasm2js";
 // import { match3_fn } from './match3helper.js';
 
-function cyrb128(str) {
-    let h1 = 1779033703, h2 = 3144134277,
-        h3 = 1013904242, h4 = 2773480762;
-    for (let i = 0, k; i < str.length; i++) {
-        k = str.charCodeAt(i);
-        h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
-        h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
-        h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
-        h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
-    }
-    h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
-    h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
-    h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
-    h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
-    h1 ^= (h2 ^ h3 ^ h4), h2 ^= h1, h3 ^= h1, h4 ^= h1;
-    return [h1 >>> 0, h2 >>> 0, h3 >>> 0, h4 >>> 0];
-}
+// function cyrb128(str) {
+//     let h1 = 1779033703, h2 = 3144134277,
+//         h3 = 1013904242, h4 = 2773480762;
+//     for (let i = 0, k; i < str.length; i++) {
+//         k = str.charCodeAt(i);
+//         h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+//         h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+//         h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+//         h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+//     }
+//     h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+//     h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+//     h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+//     h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+//     h1 ^= (h2 ^ h3 ^ h4), h2 ^= h1, h3 ^= h1, h4 ^= h1;
+//     return [h1 >>> 0, h2 >>> 0, h3 >>> 0, h4 >>> 0];
+// }
 
-function sfc32(a, b, c, d) {
-    return function () {
-        a |= 0; b |= 0; c |= 0; d |= 0;
-        var t = (a + b | 0) + d | 0;
-        d = d + 1 | 0;
-        a = b ^ b >>> 9;
-        b = c + (c << 3) | 0;
-        c = (c << 21 | c >>> 11);
-        c = c + t | 0;
-        return (t >>> 0) / 4294967296;
-    }
-}
+// function sfc32(a, b, c, d) {
+//     return function () {
+//         a |= 0; b |= 0; c |= 0; d |= 0;
+//         var t = (a + b | 0) + d | 0;
+//         d = d + 1 | 0;
+//         a = b ^ b >>> 9;
+//         b = c + (c << 3) | 0;
+//         c = (c << 21 | c >>> 11);
+//         c = c + t | 0;
+//         return (t >>> 0) / 4294967296;
+//     }
+// }
 
-function initialize_random() {
-    const seed = cyrb128("apples");
-    const rand = sfc32(seed[0], seed[1], seed[2], seed[3]);
-    return rand;
-}
+// function initialize_random() {
+//     const seed = cyrb128("apples");
+//     const rand = sfc32(seed[0], seed[1], seed[2], seed[3]);
+//     return rand;
+// }
 
 export const setupMatch3AotDemo = async (compiledJSCode: string) => {
     console.log('setting up the AOT compiled match 3 demo');
@@ -72,21 +72,21 @@ export const setupMatch3AotDemo = async (compiledJSCode: string) => {
         outputE.appendChild(s);
     }
 
-    const my_prng = initialize_random();
-    let _my_logger_counter = 0;
-    const mylogger = (...args) => {
-        const id = args[0];
-        const rest = JSON.stringify(args.slice(1));
-        console.log(`[LOG ${_my_logger_counter++} ${id}] ${rest}`);
-      };
-    // const importObject: any = { 'Math': { 'random': Math.random } };
-    const importObject: any = { 
-        'Math': { 'random': my_prng },
-        'debug': {
-            'loggeri32': mylogger,
-            'loggerf32': mylogger,
-        }
-     };
+    // const my_prng = initialize_random();
+    // let _my_logger_counter = 0;
+    // const mylogger = (...args) => {
+    //     const id = args[0];
+    //     const rest = JSON.stringify(args.slice(1));
+    //     console.log(`[LOG ${_my_logger_counter++} ${id}] ${rest}`);
+    //   };
+    const importObject: any = { 'Math': { 'random': Math.random } };
+    // const importObject: any = {
+    //     'Math': { 'random': my_prng },
+    //     'debug': {
+    //         'loggeri32': mylogger,
+    //         'loggerf32': mylogger,
+    //     }
+    // };
     // const instance = (await WebAssembly.instantiateStreaming( fetch('/assets/wasm/fire2.wasm'), imports)).instance;
     // const instance = (await WebAssembly.instantiateStreaming( fetch('/assets/wasm/fire.wasm'), imports)).instance;
     // const instance = await instantiate(compiledModule, imports);
