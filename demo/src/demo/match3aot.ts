faa19192d54b2d109cd6de3fea44be3deef8c4aa
Hari <harikrishmenon@gmail.com>
2024-02-02 18:13:12 +0530

    feat: working match3 game demo (#1)
    feat: seedable prng for deterministic results for debugging
    
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- demo/src/demo/match3aot.ts
+++ demo/src/demo/match3aot.ts
@@ -1,6 +1,43 @@
 import { ELEM_ID } from "./common";
 import { instantiateAot } from "@haribala/wasm2js";
-import { match3_fn } from './match3helper.js';
+// import { match3_fn } from './match3helper.js';
+
+function cyrb128(str) {
+    let h1 = 1779033703, h2 = 3144134277,
+        h3 = 1013904242, h4 = 2773480762;
+    for (let i = 0, k; i < str.length; i++) {
+        k = str.charCodeAt(i);
+        h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+        h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+        h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+        h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+    }
+    h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+    h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+    h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+    h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+    h1 ^= (h2 ^ h3 ^ h4), h2 ^= h1, h3 ^= h1, h4 ^= h1;
+    return [h1 >>> 0, h2 >>> 0, h3 >>> 0, h4 >>> 0];
+}
+
+function sfc32(a, b, c, d) {
+    return function () {
+        a |= 0; b |= 0; c |= 0; d |= 0;
+        var t = (a + b | 0) + d | 0;
+        d = d + 1 | 0;
+        a = b ^ b >>> 9;
+        b = c + (c << 3) | 0;
+        c = (c << 21 | c >>> 11);
+        c = c + t | 0;
+        return (t >>> 0) / 4294967296;
+    }
+}
+
+function initialize_random() {
+    const seed = cyrb128("apples");
+    const rand = sfc32(seed[0], seed[1], seed[2], seed[3]);
+    return rand;
+}
 
 export const setupMatch3AotDemo = async (compiledJSCode: string) => {
     console.log('setting up the AOT compiled match 3 demo');
@@ -35,13 +72,27 @@ export const setupMatch3AotDemo = async (compiledJSCode: string) => {
         outputE.appendChild(s);
     }
 
-    const importObject: any = { Math };
+    const my_prng = initialize_random();
+    let _my_logger_counter = 0;
+    const mylogger = (...args) => {
+        const id = args[0];
+        const rest = JSON.stringify(args.slice(1));
+        console.log(`[LOG ${_my_logger_counter++} ${id}] ${rest}`);
+      };
+    // const importObject: any = { 'Math': { 'random': Math.random } };
+    const importObject: any = { 
+        'Math': { 'random': my_prng },
+        'debug': {
+            'loggeri32': mylogger,
+            'loggerf32': mylogger,
+        }
+     };
     // const instance = (await WebAssembly.instantiateStreaming( fetch('/assets/wasm/fire2.wasm'), imports)).instance;
     // const instance = (await WebAssembly.instantiateStreaming( fetch('/assets/wasm/fire.wasm'), imports)).instance;
     // const instance = await instantiate(compiledModule, imports);
-    // const instance = await instantiateAot(compiledJSCode, importObject);
-    console.log('DEBUG match3_fn', match3_fn);
-    const instance = await instantiateAot(match3_fn, importObject);
+    const instance = await instantiateAot(compiledJSCode, importObject);
+    // console.log('DEBUG match3_fn', match3_fn);
+    // const instance = await instantiateAot(match3_fn, importObject);
     console.log('match 3 instance', instance);
     //    const instance = new WebAssembly.Instance(new WebAssembly.Module(data), imports);
     if (!(instance.exports.mem instanceof Uint8Array)) throw new Error('expected an export called "mem" of type Uint8Array');
@@ -81,9 +132,11 @@ export const setupMatch3AotDemo = async (compiledJSCode: string) => {
 
     function mouseEventHandler(event: MouseEvent) {
         let [x, y] = convertEventMouseLocation(event);
+        // console.log('DEBUG javascript x, y, buttons:', x, y, event.buttons);
         input[0] = x;
         input[1] = y;
         input[2] = event.buttons;
+        // console.log('DEBUG javascript: input', JSON.stringify(input));
     }
 
     canvas.addEventListener('mousemove', mouseEventHandler);
@@ -95,6 +148,7 @@ export const setupMatch3AotDemo = async (compiledJSCode: string) => {
         requestAnimationFrame(update);
         if (running) return;
         running = true;
+        // console.log('---------------------------');
         (exports.run as Function)();
         imageData.data.set(canvasData);
         context.putImageData(imageData, 0, 0);
--- lib/src/common/constants.ts
+++ lib/src/common/constants.ts
@@ -1,2 +1,10 @@
 export const PAGE_SIZE = 65536;
 export const MY_CTZ_FN = '__my_wasm2js_helper_ctz';
+
+export const MOST_POS_U_I64 = 2n ** 64n;
+export const MOST_POS_S_I64 = (2n ** 63n) - 1n;
+export const MOST_NEG_S_I64 = -(2n ** 63n);
+
+export const MOST_POS_U_I32 = 2 ** 32;
+export const MOST_POS_S_I32 = (2 ** 31) - 1;
+export const MOST_NEG_S_I32 = -(2 ** 31);
